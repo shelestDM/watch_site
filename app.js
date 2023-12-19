0753a990fcf11172ca980f604858dc7cf5f57887
bartashevich <shelest.dm@icloud.com>
2023-12-19 20:10:11 +0200

    slider done

---
+++ app.js
@@ -0,0 +1,33 @@
+let leftBtn = document.getElementById("left");
+let rightBtn = document.getElementById("right");
+let slides = document.querySelectorAll(".slide");
+
+let count = 0;
+
+left.addEventListener("click", () => {
+  count++;
+  MoveSlides(count);
+  console.log('lef');
+});
+
+right.addEventListener("click", () => {
+  count--;
+  MoveSlides(count);
+  console.log('right');
+});
+
+
+function MoveSlides(iterator) {
+  if (iterator === slides.length-1) {
+    count = 0;
+  } else if (iterator < 0) {
+    count = slides.length - 2;
+  }
+  TranslateSlides();
+}
+
+function TranslateSlides() {
+  slides.forEach((slide) => {
+    slide.style.transform = `translateX(calc( ${-count * 100}%))`;
+  });
+}
